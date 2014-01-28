ea1f1ed01c0639abe422322494ed35d6b11daa53
Ali Khatami <ali.d.khatami@gmail.com>
2014-01-28 00:47:29 -0700

    Added ResolveUrl() inline script and leveraged it for all current AJAX references. This will allow it to work wherever it is hosted.

---
+++ MiraclesForMito/Utilities/Util.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiraclesForMito.Utilities
+{
+	public class Util
+	{
+		public static string ResolveUrlInlineScript
+		{
+			get
+			{
+				return "String.prototype.ResolveUrl = function(){ var sbaseUrl = '" + (VirtualPathUtility.ToAbsolute("~/")) + "'; return this.replace('~/', sbaseUrl); }";
+			}
+		}
+	}
+}
