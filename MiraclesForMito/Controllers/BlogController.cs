0a7ea2cfe3f064fa425af5a633c53f8a33b4d60f
Ali Khatami <ali.d.khatami@gmail.com>
2014-03-02 01:54:45 -0700

    Rough version of pagination for the public blog pages.

---
+++ MiraclesForMito/Controllers/BlogController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiraclesForMito.Models;
+using Newtonsoft.Json;
+
+namespace MiraclesForMito.Controllers
+{
+    public class BlogController : Controller
+    {
+		SiteDB db = new SiteDB();
+
+        public ActionResult Index(BlogPaginationModel model)
+        {
+			model.AJAXUrl = model.AJAXUrl ?? "~/Blog/Paginate";
+			return View(model);
+        }
+
+		public ActionResult Paginate(PaginationModel model)
+		{
+			return PartialView(
+				"~/Views/Blog/BlogPostsPaginationBody.cshtml",
+				new BlogPaginationModel(db, model.PageIndex, model.PageSize)
+			);
+		}
+
+		public ActionResult Post()
+		{
+			string sPostTitle = RouteData.Values["id"].ToString();
+			return Index(new BlogPaginationModel(db, BlogFilterType.SEOTitle, sPostTitle));
+		}
+
+		public ActionResult Author()
+		{
+			string sAuthorName = RouteData.Values["id"].ToString();
+			var model = new BlogPaginationModel(db, BlogFilterType.Author, sAuthorName);
+			model.AJAXUrl = "~/Blog/AuthorPaginate";
+			return View("Index", model);
+		}
+
+		public ActionResult AuthorPaginate(PaginationModel model)
+		{
+			// deserialize the data
+			var additionalData = JsonConvert.DeserializeObject<Dictionary<string, string>>(model.AdditionalData);
+
+			// cast the value to the event type we want
+			BlogFilterType filterType = (BlogFilterType)int.Parse(additionalData["FilterType"]);
+			string filterValue = additionalData["FilterValue"];
+
+			return PartialView(
+				"~/Views/Blog/BlogPostsPaginationBody.cshtml",
+				new BlogPaginationModel(db, filterType, filterValue, model.PageIndex, model.PageSize)
+			);
+		}
+    }
+}
