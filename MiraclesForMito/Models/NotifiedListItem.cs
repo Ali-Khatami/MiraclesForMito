d40f7cf4bcbf3b50984539695087fff045f87707
Ali Khatami <ali.d.khatami@gmail.com>
2014-03-04 01:07:29 -0700

    - Fixed some of the breakpoints with the nav -- Added a smaller image to accommodate the break points - Added Subscribe and Unsubscribe features to the Blog - Added some better UI handling to the searching and subscribe features - Added logic to email users when a blog is created as published or updated to published from not published - The scrollspy is still broken with all of the changes

---
+++ MiraclesForMito/Models/NotifiedListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiraclesForMito.Models
+{
+	public class NotifiedListItem
+	{
+		/// <summary>
+		/// The ID of the item
+		/// </summary>
+		public int ID { get; set; }
+
+		/// <summary>
+		/// Is required and must be a valid email.
+		/// </summary>
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+	}
+}
