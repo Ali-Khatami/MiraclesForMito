2b6520968f3b88557bce002427989f0b175189d5
Tim O'Brien <tobrien772002@yahoo.com>
2014-03-04 18:43:44 -0700

    Reverse merged my changes

--- MiraclesForMito/Controllers/BlogPostController.cs
+++ MiraclesForMito/Controllers/BlogPostController.cs
@@ -9,7 +9,6 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using MiraclesForMito.Models;
-using MiraclesForMito.Utilities;
 
 namespace MiraclesForMito.Controllers
 {
@@ -29,10 +28,6 @@ namespace MiraclesForMito.Controllers
 			{
 				return Request.CreateResponse(HttpStatusCode.BadRequest);
 			}
-
-			// figure out if the post was previously published
-			// we have to use a new context to mess with the states
-			bool bWasThePostPreviouslyPublished = new SiteDB().BlogPosts.Find(id).Published.GetValueOrDefault(false);
 			
 			// set the last updated date.
 			post.UpdatedDate = DateTime.Now;
@@ -50,16 +45,6 @@ namespace MiraclesForMito.Controllers
 			try
 			{
 				db.SaveChanges();
-
-				// if the post was not previously published, but now is, we need to send emails to those
-				// that were signed up to receive emails.
-				if (!bWasThePostPreviouslyPublished && post.Published.GetValueOrDefault(false))
-				{
-					foreach (var notificationItem in db.NotifiedList)
-					{
-						SendPostedEmail(notificationItem.Email, post);
-					}
-				}
 			}
 			catch (DbUpdateConcurrencyException ex)
 			{
@@ -81,16 +66,6 @@ namespace MiraclesForMito.Controllers
 
 				HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, post);
 				response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = post.ID }));
-
-				// if the post is saved and the post has published set to true we need to send emails
-				if (post != null && post.Published.GetValueOrDefault(false))
-				{
-					foreach (var notificationItem in db.NotifiedList)
-					{
-						SendPostedEmail(notificationItem.Email, post);
-					}
-				}
-
 				return response;
 			}
 			else
@@ -121,23 +96,6 @@ namespace MiraclesForMito.Controllers
 			}
 
 			return Request.CreateResponse(HttpStatusCode.OK, post);
-		}
-
-		private void SendPostedEmail(string email, BlogPost post)
-		{
-			EmailHelpers.SendEmail(
-				new System.Net.Mail.MailMessage(EmailHelpers.SEND_EMAIL_ADDRESS, email)
-				{
-					Subject = "Miracles for Mito New Blog Post!",
-					Body = "The Miracles for Mito Blog team has posted a new blog post, <a title=\"Click here to visit the Miracles for Mito Blog\" href=\"" +
-						(string.Format("~/Blog/Post/{0}", post.SEOLink)).ToAbsoluteUrl() +
-					"\">" + post.Title + "</a>.<br/><br/>" +
-					@"Sincerely,<br/>
-					The Miracles for Mito Dev Team<br/><br/>" +
-					"<a href=\"" + (string.Format("~/Unsubscribe/{0}", SimpleCrypto.Encrypt(email))).ToAbsoluteUrl() + "\">Click here to unsubscribe from these emails</a>",
-					IsBodyHtml = true
-				}
-			);
 		}
 
 		protected override void Dispose(bool disposing)
